0819ad78fcabd0e8e2af22e7b61de0fb236fe92f
moledoc <meelis.utt@gmail.com>
2021-04-12 22:47:20 +0300

    changed N and P value for mc_amer runscript

--- runscript_mc_amer.sh
+++ runscript_mc_amer.sh
@@ -19,16 +19,16 @@ do
     echo "#pragma once
 double comparison = $(./bin/binom_vanilla_amer ${payoff_fun} ${S0} ${E} ${r} ${sigma} ${T} 1000 | tr ',' '\t' | awk '{print $13}');" > include/comparison.h
     make mc_amer_bin
-    for N in 1 10 #100 #1000 # in thousands # paths
+    for N in 10 #100 #1000 # in thousands # paths
     do
-      for M in 1 10 #100 # in hundreds # steps in paths
+      for M in 10 #100 # in hundreds # steps in paths
       do
         ./bin/mc_amer ${payoff_fun} ${S0} ${E} ${r} ${sigma} ${T} ${N}000 ${M}00 >> ${results} 
         for thread in 1 2 4 8 #16 32 64 #128
         do
           ./bin/mc_amer_omp ${payoff_fun} ${S0} ${E} ${r} ${sigma} ${T} ${N}000 ${M}00 ${thread} >> ${results}
         done
-        for p in 1 2 4 #8 #16 32 64 #128
+        for p in 1 2 4 8 #16 32 64 #128
         do
           mpirun -n $p --hostfile hostfile ./bin/mc_amer_mpi ${payoff_fun} ${S0} ${E} ${r} ${sigma} ${T} ${N}000 ${M}00 >> ${results}
           for thread in 1 2 4 8 #16 32 64 #128
